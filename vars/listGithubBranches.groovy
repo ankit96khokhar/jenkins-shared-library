373ae982a239935b1023393a3a95e570dfc89a0a
Ankit Khokhar <ankitkhokhar@Mac.lan>
2026-02-07 00:50:24 +0530

    jenkins

--- vars/listGithubBranches.groovy
+++ vars/listGithubBranches.groovy
@@ -4,19 +4,14 @@ def call(String owner, String repo) {
 
   def output = sh(
     script: """
-      curl -s -H "Authorization: token \$GITHUB_API_TOKEN" \
-        ${apiUrl} \
-      | grep '"name"' \
-      | cut -d '"' -f4
+      curl -i -s -H "Authorization: token \$GITHUB_API_TOKEN" \
+        ${apiUrl}
     """,
     returnStdout: true
   ).trim()
 
-  if (!output) {
-    return ["NO_BRANCHES_FOUND"]
-  }
+  echo "RAW GITHUB RESPONSE:"
+  echo output
 
-  def branches = output.split("\\n")
-
-  return branches.size() > 0 ? branches : ["NO_BRANCHES_FOUND"]
+  return ["DEBUG_MODE"]
 }

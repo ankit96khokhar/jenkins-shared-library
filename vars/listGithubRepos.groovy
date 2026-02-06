2f0ce41e4889115619f6ddfb0efecff9c897c936
Ankit Khokhar <ankitkhokhar@Mac.lan>
2026-02-07 00:39:28 +0530

    jenkins

--- vars/listGithubRepos.groovy
+++ vars/listGithubRepos.groovy
@@ -1,24 +1,18 @@
 def call(String username) {
+  withCredentials([string(credentialsId: 'github-api-token', variable: 'TOKEN')]) {
 
-  def apiUrl = "https://api.github.com/users/${username}/repos?per_page=100"
+    def apiUrl = "https://api.github.com/users/${username}/repos?per_page=100"
 
-  def output = sh(
-    script: """
-      curl -s -H "Authorization: token \$GITHUB_API_TOKEN" \
-        ${apiUrl} \
-      | grep '"name"' \
-      | cut -d '"' -f4
-    """,
-    returnStdout: true
-  ).trim()
+    def output = sh(
+      script: """
+        curl -s -H "Authorization: token \$TOKEN" \
+          ${apiUrl} \
+        | grep '"name"' \
+        | cut -d '"' -f4
+      """,
+      returnStdout: true
+    ).trim()
 
-  // 🔥 CRITICAL: Active Choice MUST return non-empty list
-  if (!output) {
-    return ["NO_REPOS_FOUND"]
+    return output ? output.split("\n") : []
   }
-
-  def repos = output.split("\\n")
-
-  // Extra safety
-  return repos.size() > 0 ? repos : ["NO_REPOS_FOUND"]
 }

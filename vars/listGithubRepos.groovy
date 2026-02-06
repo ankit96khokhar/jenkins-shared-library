e2efe3888cf0ddef86057250c915e513ac7f6629
Ankit Khokhar <ankitkhokhar@Mac.lan>
2026-02-06 23:45:29 +0530

    jenkins

--- vars/listGithubRepos.groovy
+++ vars/listGithubRepos.groovy
@@ -1,13 +1,15 @@
 def call(String username) {
   withCredentials([string(credentialsId: 'github-api-token', variable: 'TOKEN')]) {
 
+    def apiUrl = "https://api.github.com/users/${username}/repos?per_page=100"
+
     def output = sh(
-      script: '''
-        curl -s -H "Authorization: token $TOKEN" \
-          https://api.github.com/users/'"${username}"'/repos?per_page=100 \
+      script: """
+        curl -s -H "Authorization: token \$TOKEN" \
+          ${apiUrl} \
         | grep '"name"' \
         | cut -d '"' -f4
-      ''',
+      """,
       returnStdout: true
     ).trim()
 

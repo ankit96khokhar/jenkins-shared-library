a38bdbc6ba0ee4f201770bca26889a1e90a6a101
Ankit Khokhar <ankitkhokhar@Mac.lan>
2026-02-06 23:33:27 +0530

    jenkins

--- vars/listGithubRepos.groovy
+++ vars/listGithubRepos.groovy
@@ -1,14 +1,16 @@
 def call(String username) {
-    withCredentials([string(credentialsId: 'github-api-token', variable: 'TOKEN')]) {
-        def cmd = """
-            curl -s -H "Authorization: token ${TOKEN}" \
-            https://api.github.com/users/${username}/repos?per_page=100 \
-            | grep '"name"' \
-            | cut -d '"' -f4   
-        """
+  withCredentials([string(credentialsId: 'github-api-token', variable: 'TOKEN')]) {
 
-        def output = sh(script: cmd, returnStdout: true).trim()
+    def output = sh(
+      script: '''
+        curl -s -H "Authorization: token $TOKEN" \
+          https://api.github.com/users/'"${username}"'/repos?per_page=100 \
+        | grep '"name"' \
+        | cut -d '"' -f4
+      ''',
+      returnStdout: true
+    ).trim()
 
-        return output ? output.split("\n") : []
-    }
+    return output ? output.split("\n") : []
+  }
 }
